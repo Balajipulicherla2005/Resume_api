b4f9fbc37852ff720d71a84a17e2bf6d5e9f1520
Balajipulicherla2005 <Balajipulicherla941@gmail.com>
2026-06-12 19:29:13 +0530

    first commit

--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,28 +1,3 @@
-// import express from "express";
-
-// import {
-//   saveResume,
-//   getResumes,
-// } from "../controllers/resumeController.js";
-
-// const router = express.Router();
-
-// router.post("/save", saveResume);
-
-// router.get("/", getResumes);
-
-// export default router;
-
-
-
-
-
-
-
-
-
-
-
 import express
 from "express";
 
